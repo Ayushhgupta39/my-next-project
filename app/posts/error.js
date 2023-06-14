95072bb0e17b32e80659c89842f3bc0c38f04b6d
Ayush Gupta <ayushgupta3902@gmail.com>
2023-06-14 23:39:03 +0530

    Understand file structure

---
+++ app/posts/error.js
@@ -0,0 +1,13 @@
+// Error components must be client components
+"use client";
+
+
+import React from 'react'
+
+export default function error() {
+  return (
+    <div>
+      Error file
+    </div>
+  )
+}
