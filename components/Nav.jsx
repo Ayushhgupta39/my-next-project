dcf18f86f5e0e6ac930c179defb626c4d0f89b19
Ayush Gupta <ayushgupta3902@gmail.com>
2023-06-17 00:20:07 +0530

    Write route.js

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,19 +6,19 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 import Image from "next/image";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     };
 
-    setProviders();
+    setUpProviders();
   }, []);
 
   return (
@@ -29,13 +29,14 @@ const Nav = () => {
           width={30}
           height={30}
           className="object-contain"
+          alt="logo"
         />
         <p className="logo_text">Promptopia</p>
       </Link>
 
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -47,10 +48,11 @@ const Nav = () => {
 
             <Link href={"/profile"}>
               <Image
-                src={"/assets/images/logo.svg"}
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
+                alt="profile"
               />
             </Link>
           </div>
@@ -61,7 +63,7 @@ const Nav = () => {
                 <button
                   type="button"
                   key={providers.name}
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => signIn(providers.id)}
                   className="black_btn"
                 >
                   Sign In
@@ -73,10 +75,10 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={30}
               height={30}
               className="object-contain"
@@ -116,7 +118,7 @@ const Nav = () => {
                 <button
                   type="button"
                   key={providers.name}
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => signIn(providers.id)}
                   className="black_btn"
                 >
                   Sign In
--- utils/database.js
+++ utils/database.js
@@ -11,8 +11,15 @@ export const connectToDB = async () => {
     }
 
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
-    } catch (error) {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "share_prompt",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
 
+        isConnected = true
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.log(error);
     }
 }
